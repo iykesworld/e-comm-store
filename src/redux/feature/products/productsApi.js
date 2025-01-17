bee23678bf6120489d85575f9cc154bf9cb97f7d
iykesworld <kennethiyke12@gmail.com>
2025-01-17 00:52:09 +0000

    fixed the baseUrl

--- src/redux/feature/products/productsApi.js
+++ src/redux/feature/products/productsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/products",
+    baseUrl: "https://e-comm-server-five.vercel.app/api/products",
     credentials: "include",
   }),
   tagTypes: ["Products"],
